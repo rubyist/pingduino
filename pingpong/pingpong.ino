75a2715879c695846960f921217e2c523f5fe9a6
rubyist <scott.barron@github.com>
2013-12-22 15:57:49 -0500

    basic game winning rule

--- pingpong/pingpong.ino
+++ pingpong/pingpong.ino
@@ -1,5 +1,6 @@
 const int p1Pin = 2;
 const int p2Pin = 3;
+
 int p1Score = 0;
 int p2Score = 0;
 
@@ -21,6 +22,7 @@ void loop()
     if (p1State == LOW) {
       p1Score++;
       showScore();
+      checkForWinner();
     }
   }
   lastp1State = p1State;
@@ -30,6 +32,7 @@ void loop()
     if (p2State == LOW) {
       p2Score++;
       showScore();
+      checkForWinner();
     }
   }
   lastp2State = p2State;
@@ -41,4 +44,28 @@ void showScore()
   Serial.print(p1Score);
   Serial.print("  P2: ");
   Serial.println(p2Score);
+}
+
+void checkForWinner()
+{
+  if ((p1Score >= 11 || p2Score >= 11) && abs(p1Score - p2Score) >= 2) {
+
+    Serial.print("*** WINNER ***   ");
+    if (p1Score > p2Score) {
+      Serial.print("P1");
+    } else {
+      Serial.print("P2");
+    }
+    Serial.println("   *** WINNER ***");
+    
+    resetGame();
+    showScore();
+  }
+}
+
+void resetGame()
+{
+  p1Score = 0;
+  p2Score = 0;
+  gameOver = 0;
 }
