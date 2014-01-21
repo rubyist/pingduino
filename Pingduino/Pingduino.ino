faf209e51484c13da35993280de6d9225abae68b
rubyist <scott.barron@github.com>
2014-01-20 21:57:00 -0500

    First pass at incorporating WaveHC and Wave Shield

--- Pingduino/Pingduino.ino
+++ Pingduino/Pingduino.ino
@@ -1,11 +1,11 @@
 #include "Game.h"
 #include "Display.h"
-#include "VictorySong.h"
+#include "WaveUtil.h"
+#include "WaveHC.h"
 
 #define P1PIN           0 // Interrupts
 #define P2PIN           1
 #define BUTTONLIGHTPIN  4
-#define SPEAKERPIN      9
 #define LONGPRESS_TIME  1000
 #define INACTIVEMILLIS  1200000 // 20 minutes
 
@@ -17,9 +17,121 @@ volatile unsigned long lastActivityTime;
 
 boolean sleeping = false;
 
+class VictorySong {
+  public:
+    VictorySong(int pin);
+    void init();
+    void play();
+    bool played();
+    void reset();
+    void stop();
+  
+  protected:
+    const int pin;
+    bool _played;
+    bool _stop;
+    bool _initialized;
+
+    SdReader card;
+    FatVolume vol;
+    FatReader root;
+    uint8_t dirLevel;
+    dir_t dirBuf;
+    WaveHC wave;
+};
+
+VictorySong::VictorySong(int p) : pin(p)
+{
+}
+
+void VictorySong::init()
+{
+  _played = false;
+  _stop = false;
+
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+
+  if (!card.init()) {
+    Serial.println("Card init failed.");
+    _initialized = false;
+    return;
+  }
+
+  card.partialBlockRead(true);
+
+  uint8_t part;
+  for (part = 0; part < 5; part++) {
+    if (vol.init(card, part))
+      break;
+  }
+
+  if (part == 5) {
+    Serial.println("No valid FAT partition");
+    _initialized = false;
+    return;
+  }
+
+  if (!root.openRoot(vol)) {
+    Serial.println("Can't open root directory");
+    _initialized = false;
+    return;
+  }
+  _initialized = true;
+}
+
+void VictorySong::play()
+{
+  if (!_initialized)
+    return;
+      
+  _played = true;
+  
+  // Grab the first wav file and play it
+  FatReader file;
+  while (root.readDir(dirBuf) > 0) {
+    Serial.println(*dirBuf.name);
+    
+    if (dirBuf.name[0] == '.')
+      continue;
+    
+    if (!file.open(vol, dirBuf)) {
+      Serial.println("Failed to open the file");
+      return;
+    }
+    
+    if (!file.isDir()) {
+      if (wave.create(file)) {
+        wave.play();
+        while(wave.isplaying) {
+          delay(100);
+        }
+      } else {
+        Serial.println("Failed to create wav");
+      }
+    }
+  }  
+}
+
+void VictorySong::stop()
+{
+  _stop = true;
+}
+
+bool VictorySong::played() { return _played; }
+
+void VictorySong::reset()
+{
+  _played = false;
+  _stop = false;
+}
+
+
 Game game;
 Display display;
-VictorySong victorySong(SPEAKERPIN);
+VictorySong victorySong(2);
 
 void setup()
 {
