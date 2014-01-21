8fd823ab3d0038eb609f218f3a26cc42ee339246
rubyist <scott.barron@github.com>
2014-01-20 22:44:18 -0500

    Move victory song back into its own files

--- Pingduino/Pingduino.ino
+++ Pingduino/Pingduino.ino
@@ -1,7 +1,9 @@
-#include "Game.h"
-#include "Display.h"
 #include "WaveUtil.h"
 #include "WaveHC.h"
+
+#include "Game.h"
+#include "Display.h"
+#include "VictorySong.h"
 
 #define P1PIN           0 // Interrupts
 #define P2PIN           1
@@ -16,116 +18,6 @@ volatile int requestReset = 0; // 0 = nobody, 1 = p1, 2 = p2
 volatile unsigned long lastActivityTime;
 
 boolean sleeping = false;
-
-class VictorySong {
-  public:
-    VictorySong();
-    void init();
-    void play();
-    bool played();
-    void reset();
-    void stop();
-  
-  protected:
-    volatile bool _played;
-    bool _stop;
-    bool _initialized;
-
-    SdReader card;
-    FatVolume vol;
-    FatReader root;
-    uint8_t dirLevel;
-    dir_t dirBuf;
-    WaveHC wave;
-};
-
-VictorySong::VictorySong()
-{
-}
-
-void VictorySong::init()
-{
-  _played = false;
-  _stop = false;
-
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-
-  if (!card.init()) {
-    Serial.println("Card init failed.");
-    _initialized = false;
-    return;
-  }
-
-  card.partialBlockRead(true);
-
-  uint8_t part;
-  for (part = 0; part < 5; part++) {
-    if (vol.init(card, part))
-      break;
-  }
-
-  if (part == 5) {
-    Serial.println("No valid FAT partition");
-    _initialized = false;
-    return;
-  }
-
-  _initialized = true;
-}
-
-void VictorySong::play()
-{
-  if (!_initialized)
-    return;
-      
-  _played = true;
-  
-  // Grab the first wav file and play it
-  FatReader file;
-
-  if (!root.openRoot(vol)) {
-    Serial.println("Can't open root directory");
-    return;
-  }
-
-  while (root.readDir(dirBuf) > 0) {
-    if (dirBuf.name[0] == '.')
-      continue;
-    
-    if (!file.open(vol, dirBuf)) {
-      Serial.println("Failed to open the file");
-      return;
-    }
-    
-    if (!file.isDir()) {
-      if (wave.create(file)) {
-        wave.play();
-        while(wave.isplaying) {
-          delay(100);
-        }
-        return;
-      }
-    }
-  }  
-}
-
-void VictorySong::stop()
-{
-  _stop = true;
-  wave.stop();
-}
-
-bool VictorySong::played() { return _played; }
-
-void VictorySong::reset()
-{
-  _played = false;
-  _stop = false;
-}
-
 
 Game game;
 Display display;
