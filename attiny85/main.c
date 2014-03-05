c2eef7eb67a6dddbea2997d725a5d0291321fed3
rubyist <scott.barron@github.com>
2014-03-05 07:59:26 -0500

    Rudimentary scoring button code Does not yet contain the state machine for short/long/double pressing

--- attiny85/main.c
+++ attiny85/main.c
@@ -3,17 +3,64 @@
 #include <util/delay.h>
 #include "display.h"
 
+#define DEBOUNCE_TIME 1000
+
+/*
+PB0 - unassigned
+PB1 - display
+PB2 - unassigned
+PB3 - p1 score
+PB4 - p2 score
+PB5 - unassgined
+*/
+
+#define P1BUTTON PB3
+#define P2BUTTON PB4
+
+uint8_t debounce(uint8_t button) {
+  if (bit_is_clear(PINB, button)) {
+    _delay_us(DEBOUNCE_TIME);
+    if (bit_is_clear(PINB, button)) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
 int main(void) {
+  uint8_t p1buttonWasPressed = 0;
+  uint8_t p1Score = 0;
+  uint8_t p2buttonWasPressed = 0;
+  uint8_t p2Score = 0;
+
+
+  PORTB |= (1 << P1BUTTON) | (1 << P2BUTTON);
+
   CLKPR=_BV(CLKPCE);
   CLKPR=0;
 
-  int n;
-
   while (1) {
-    for (n = 0; n < 12; n++) {
-      display_score(n, n);
-      _delay_ms(1000);
+    if (debounce(P1BUTTON)) {
+      if (p1buttonWasPressed == 0) {
+        p1Score++;
+        p1buttonWasPressed = 1;
+      }
+    } else {
+      p1buttonWasPressed = 0;
     }
+
+    if (debounce(P2BUTTON)) {
+      if (p2buttonWasPressed == 0) {
+        p2Score++;
+        p2buttonWasPressed = 1;
+      }
+    } else {
+      p2buttonWasPressed = 0;
+    }
+
+
+    display_score(p1Score, p2Score);
+    _delay_ms(10);
   }
   return (0);
 }
